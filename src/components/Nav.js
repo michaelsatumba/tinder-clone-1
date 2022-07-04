c50e75676352dec6cd8bfc1939c555cb56f9261a
michaelsatumba <mjsatumba@gmail.com>
2022-07-04 10:45:55 -0700

    authModal UI

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,11 @@
 import whiteLogo from '../images/tinder_logo_white.png';
 import colorLogo from '../images/color-logo-tinder.png';
 
-const Nav = ({ minimal, authToken }) => {
+const Nav = ({ minimal, authToken, setShowModal, showModal, setIsSignUp }) => {
+	const handleClick = () => {
+		setShowModal(true);
+		setIsSignUp(false);
+	};
 	return (
 		<nav>
 			<div className="logo-container">
@@ -14,8 +18,8 @@ const Nav = ({ minimal, authToken }) => {
 			{!authToken && !minimal && (
 				<button
 					className="nav-button"
-					// onClick={handleClick}
-					// disabled={showModal}
+					onClick={handleClick}
+					disabled={showModal}
 				>
 					Log in
 				</button>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,10 @@
 import Nav from '../components/Nav';
+import AuthModal from '../components/AuthModal';
+import { useState } from 'react';
 
 const Home = () => {
+	const [showModal, setShowModal] = useState(false);
+	const [isSignUp, setIsSignUp] = useState(true);
 	const authToken = false;
 
 	const handleClick = () => {
@@ -11,18 +15,28 @@ const Home = () => {
 		// 	window.location.reload();
 		// 	return;
 		// }
-		// setShowModal(true);
-		// setIsSignUp(true);
+		setShowModal(true);
+		setIsSignUp(true);
 	};
 
 	return (
 		<div className="overlay">
-			<Nav minimal={false} authToken={authToken} />
+			<Nav
+				minimal={false}
+				authToken={authToken}
+				setShowModal={setShowModal}
+				showModal={showModal}
+				setIsSignUp={setIsSignUp}
+			/>
 			<div className="home">
 				<h1 className="primary-title">Swipe Right®</h1>
 				<button className="primary-button" onClick={handleClick}>
 					{authToken ? 'Signout' : 'Create Account'}
 				</button>
+
+				{showModal && (
+					<AuthModal setShowModal={setShowModal} isSignUp={isSignUp} />
+				)}
 			</div>
 		</div>
 	);
