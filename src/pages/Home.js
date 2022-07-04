e5868ed9e2fc9327c5c38a41480dea16c6df8d8a
michaelsatumba <mjsatumba@gmail.com>
2022-07-04 09:59:57 -0700

    homepage & navbar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,31 @@
+import Nav from '../components/Nav';
+
 const Home = () => {
-	return <div>Home</div>;
+	const authToken = false;
+
+	const handleClick = () => {
+		console.log('clicked');
+		// if (authToken) {
+		// 	removeCookie('UserId', cookies.UserId);
+		// 	removeCookie('AuthToken', cookies.AuthToken);
+		// 	window.location.reload();
+		// 	return;
+		// }
+		// setShowModal(true);
+		// setIsSignUp(true);
+	};
+
+	return (
+		<div className="overlay">
+			<Nav minimal={false} authToken={authToken} />
+			<div className="home">
+				<h1 className="primary-title">Swipe Right®</h1>
+				<button className="primary-button" onClick={handleClick}>
+					{authToken ? 'Signout' : 'Create Account'}
+				</button>
+			</div>
+		</div>
+	);
 };
 
 export default Home;
