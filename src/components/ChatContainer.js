d6eaa7890d4c87ebc70e21cb99a1439c1ee58431
michaelsatumba <mjsatumba@gmail.com>
2022-07-05 10:09:30 -0700

    chatContainer UI

--- src/components/ChatContainer.js
+++ src/components/ChatContainer.js
@@ -1,13 +1,33 @@
-// import ChatHeader from './ChatHeader'
-// import MatchesDisplay from './MatchesDisplay'
-// import ChatDisplay from './ChatDisplay'
+import ChatHeader from './ChatHeader';
+import MatchesDisplay from './MatchesDisplay';
+import ChatDisplay from './ChatDisplay';
 // import { useState } from 'react'
 
 const ChatContainer = ({ user }) => {
 	// const [ clickedUser, setClickedUser ] = useState(null)
 
 	return (
-		<div className="chat-container">Chat Container</div>
+		<div className="chat-container">
+			<ChatHeader
+			// user={user}
+			/>
+			<div>
+				<button
+					className="option"
+					// onClick={() => setClickedUser(null)}
+				>
+					Matches
+				</button>
+				<button
+					className="option"
+					// disabled={!clickedUser}
+				>
+					Chat
+				</button>
+			</div>
+			<MatchesDisplay />
+			<ChatDisplay />
+		</div>
 		// <div className="chat-container">
 		//     <ChatHeader user={user}/>
 
